6a676199a06ff80cc63a4b175de5b8b02be3b16e
Stephen Sviatko <ssviatko@gmail.com>
2025-09-28 20:25:45 -0700

    make colorterm_test print everything on one page

--- colorterm_test.cc
+++ colorterm_test.cc
@@ -5,84 +5,81 @@
 
 int main(int argc, char **argv)
 {
-	std::cout << "standard PC colors (iterated from array)" << std::endl;
+	std::cout << "standard PC colors                                                       Standard GS colors" << std::endl;
 	for (int i = 0;  i < 16; ++i) {
 		std::cout << ss::color[i] << " * " << ss::COLOR_DEFAULT;
+	}
+	std::cout << "                         ";
+	for (unsigned int i = 0;  i < 16; ++i) {
+		std::cout << ss::color_gs(i) << " * " << ss::COLOR_DEFAULT;
 	}
 	std::cout << std::endl;
 	for (int i = 0;  i < 16; ++i) {
 		std::cout << ss::color_bg[i] << ss::color[15] << " * " << ss::COLOR_DEFAULT;
 	}
-	std::cout << std::endl;
-	std::cout << "standard PC colors" << std::endl;
-	std::cout << ss::COLOR_RED <<          "this is a nice shade of dark red      " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_RED << ss::COLOR_BLACK <<          "black on dark red background     " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_GREEN <<        "this is a nice shade of dark green    " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_GREEN << ss::COLOR_BLACK <<        "black on dark green background   " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_YELLOW <<       "this is a nice shade of yellow        " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_YELLOW << ss::COLOR_BLACK <<       "black on dark yellow background  " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_BLUE <<         "this is a nice shade of dark blue     " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_BLUE << ss::COLOR_BLACK <<         "black on dark blue background    " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_MAGENTA <<      "this is a nice shade of dark magenta  " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_MAGENTA << ss::COLOR_BLACK <<      "black on dark magenta background " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_CYAN <<         "this is a nice shade of dark cyan     " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_CYAN << ss::COLOR_BLACK <<         "black on dark cyan background    " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_LIGHTGRAY <<    "this is a nice shade of light gray    " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_LIGHTGRAY << ss::COLOR_BLACK <<    "black on light gray background   " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_DARKGRAY <<     "this is a nice shade of dark gray     " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_DARKGRAY << ss::COLOR_BLACK <<     "black on dark gray background    " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_LIGHTRED <<     "this is a nice shade of light red     " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_LIGHTRED << ss::COLOR_BLACK <<     "black on light red background    " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_LIGHTGREEN <<   "this is a nice shade of light green   " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_LIGHTGREEN << ss::COLOR_BLACK <<   "black on light green background  " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_LIGHTYELLOW <<  "this is a nice shade of light yellow  " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_LIGHTYELLOW << ss::COLOR_BLACK <<  "black on light yellow background " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_LIGHTBLUE <<    "this is a nice shade of light blue    " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_LIGHTBLUE << ss::COLOR_BLACK <<    "black on light blue background   " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_LIGHTMAGENTA << "this is a nice shade of light magenta " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_LIGHTMAGENTA << ss::COLOR_BLACK << "black on light magenta background" << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_LIGHTCYAN <<    "this is a nice shade of light cyan    " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_LIGHTCYAN << ss::COLOR_BLACK <<    "black on light cyan background   " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << ss::COLOR_WHITE <<        "this is a nice shade of white         " << ss::COLOR_DEFAULT;
-	std::cout << ss::COLOR_BG_WHITE << ss::COLOR_BLACK <<        "black on white background        " << ss::COLOR_DEFAULT << std::endl;
-	std::cout << "standard GS colors (iterated from array)" << std::endl;
-	for (unsigned int i = 0;  i < 16; ++i) {
-		std::cout << ss::color_gs(i) << " * " << ss::COLOR_DEFAULT;
-	}
-	std::cout << std::endl;
+	std::cout << "                         ";
 	for (unsigned int i = 0;  i < 16; ++i) {
 		std::cout << ss::color_gs_bg(i) << ss::color_gs(15) << " * " << ss::COLOR_DEFAULT;
 	}
 	std::cout << std::endl;
-	std::cout << "using GS colors by name in source code" << std::endl;
+	std::cout << ss::COLOR_RED <<          "this is a nice shade of dark red      " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_RED << ss::COLOR_BLACK <<          "black on dark red background     " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("DEEPRED")) <<    "This is a nice shade of deep red    " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("DEEPRED")) << ss::color_gs(0) <<    "This is a nice shade of deep red    " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_GREEN <<        "this is a nice shade of dark green    " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_GREEN << ss::COLOR_BLACK <<        "black on dark green background   " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("DARKBLUE")) <<   "This is a nice shade of dark blue   " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("DARKBLUE")) << ss::color_gs(0) <<   "This is a nice shade of dark blue   " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_YELLOW <<       "this is a nice shade of yellow        " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_YELLOW << ss::COLOR_BLACK <<       "black on dark yellow background  " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("PURPLE")) <<     "This is a nice shade of purple      " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("PURPLE")) << ss::color_gs(0) <<     "This is a nice shade of purple      " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_BLUE <<         "this is a nice shade of dark blue     " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_BLUE << ss::COLOR_BLACK <<         "black on dark blue background    " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("DARKGREEN")) <<  "This is a nice shade of dark green  " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("DARKGREEN")) << ss::color_gs(0) <<  "This is a nice shade of dark green  " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_MAGENTA <<      "this is a nice shade of dark magenta  " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_MAGENTA << ss::COLOR_BLACK <<      "black on dark magenta background " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("DARKGRAY")) <<   "This is a nice shade of dark gray   " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("DARKGRAY")) << ss::color_gs(0) <<   "This is a nice shade of dark gray   " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_CYAN <<         "this is a nice shade of dark cyan     " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_CYAN << ss::COLOR_BLACK <<         "black on dark cyan background    " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("MEDIUMBLUE")) << "This is a nice shade of medium blue " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("MEDIUMBLUE")) << ss::color_gs(0) << "This is a nice shade of medium blue " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_LIGHTGRAY <<    "this is a nice shade of light gray    " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_LIGHTGRAY << ss::COLOR_BLACK <<    "black on light gray background   " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("LIGHTBLUE")) <<  "This is a nice shade of light blue  " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("LIGHTBLUE")) << ss::color_gs(0) <<  "This is a nice shade of light blue  " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_DARKGRAY <<     "this is a nice shade of dark gray     " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_DARKGRAY << ss::COLOR_BLACK <<     "black on dark gray background    " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("BROWN")) <<      "This is a nice shade of brown       " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("BROWN")) << ss::color_gs(0) <<      "This is a nice shade of brown       " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_LIGHTRED <<     "this is a nice shade of light red     " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_LIGHTRED << ss::COLOR_BLACK <<     "black on light red background    " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("ORANGE")) <<     "This is a nice shade of orange      " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("ORANGE")) << ss::color_gs(0) <<     "This is a nice shade of orange      " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_LIGHTGREEN <<   "this is a nice shade of light green   " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_LIGHTGREEN << ss::COLOR_BLACK <<   "black on light green background  " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("LIGHTGRAY")) <<  "This is a nice shade of light gray  " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("LIGHTGRAY")) << ss::color_gs(0) <<  "This is a nice shade of light gray  " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_LIGHTYELLOW <<  "this is a nice shade of light yellow  " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_LIGHTYELLOW << ss::COLOR_BLACK <<  "black on light yellow background " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("PINK")) <<       "This is a nice shade of pink        " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("PINK")) << ss::color_gs(0) <<       "This is a nice shade of pink        " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_LIGHTBLUE <<    "this is a nice shade of light blue    " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_LIGHTBLUE << ss::COLOR_BLACK <<    "black on light blue background   " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("LIGHTGREEN")) << "This is a nice shade of light green " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("LIGHTGREEN")) << ss::color_gs(0) << "This is a nice shade of light green " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_LIGHTMAGENTA << "this is a nice shade of light magenta " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_LIGHTMAGENTA << ss::COLOR_BLACK << "black on light magenta background" << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("YELLOW")) <<     "This is a nice shade of yellow      " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("YELLOW")) << ss::color_gs(0) <<     "This is a nice shade of yellow      " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_LIGHTCYAN <<    "this is a nice shade of light cyan    " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_LIGHTCYAN << ss::COLOR_BLACK <<    "black on light cyan background   " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("AQUAMARINE")) << "This is a nice shade of aquamarine  " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("AQUAMARINE")) << ss::color_gs(0) << "This is a nice shade of aquamarine  " << ss::COLOR_DEFAULT << std::endl;
+	std::cout << ss::COLOR_WHITE <<        "this is a nice shade of white         " << ss::COLOR_DEFAULT;
+	std::cout << ss::COLOR_BG_WHITE << ss::COLOR_BLACK <<        "black on white background        " << ss::COLOR_DEFAULT << "  ";
 	std::cout << ss::color_gs(ss::color_gs_name("WHITE")) <<      "This is a nice shade of white       " << ss::COLOR_DEFAULT;
 	std::cout << ss::color_gs_bg(ss::color_gs_name("WHITE")) << ss::color_gs(0) <<      "This is a nice shade of white       " << ss::COLOR_DEFAULT << std::endl;
 	
@@ -93,7 +90,6 @@ int main(int argc, char **argv)
 		}
 		std::cout << std::endl;
 	}
-	std::cout << ss::COLOR_DEFAULT << " " << std::endl;
 	for (unsigned int j = 0; j < 8; ++j) {
 		for (unsigned int i = 0; i < 32; ++i) {
 			std::cout << ss::color_256_bg(i + (32 * j)) << ss::color_256(15) << " " << std::setw(3) << std::setfill('0') << i + (32 * j) << " " << ss::COLOR_DEFAULT;
@@ -116,7 +112,6 @@ int main(int argc, char **argv)
 		}
 		std::cout << std::endl;
 	}
-	std::cout << std::endl;
 	
 	return 0;
 }
